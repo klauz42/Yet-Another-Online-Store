9f21b45ed578c07bbd436739906760a6019bb369
Nikolai Matveev <nmatweew@yandex.ru>
2024-02-02 15:22:27 +0400

    Forcefully disabled dark theme

--- app/src/main/java/ru/klauz42/yetanotheronlinestore/MainApp.kt
+++ app/src/main/java/ru/klauz42/yetanotheronlinestore/MainApp.kt
@@ -1,6 +1,12 @@
 package ru.klauz42.yetanotheronlinestore
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
 }
